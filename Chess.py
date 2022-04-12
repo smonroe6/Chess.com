bf32b989772135555f84e6224d7605d0c9efb06f
Stephen Monroe <smonroe6@vols.utk.edu>
2022-04-11 21:31:13 -0400

    Code maybe works
    Leaderboard said index wrong
    
    not enough games in different categories for rating
    
    most recent game works

--- Chess.py
+++ Chess.py
@@ -1,5 +1,8 @@
-from chessdotcom import get_leaderboards
+from chessdotcom import get_leaderboards, get_player_stats, get_player_game_archives
 import pprint
+import requests
+
+from requests import request
 
 printer = pprint.PrettyPrinter()
 
@@ -12,4 +15,23 @@ def print_leaderboard():
         for idx, entry in enumerate(data[category]):
             print(f'Rank: {idx+1} | Username: {entry["username"]} | Rating: {entry["score"]}')
 
-print_leaderboard()
+
+def get_player_rating(username):
+    data = get_player_stats(username).json
+    categories = ['chess_blitz', 'chess_rapid', 'chess_bullet']
+    for category in categories:
+        print('Category: ', category)
+        print(f'Current: {data[category]["last"]["rating"]}')
+        print(f'Best: {data[category]["best"]["rating"]}')
+        (f'Current: {data[category]["record"]}')
+    
+
+
+def get_most_recent_game(username):
+    data = get_player_game_archives(username).json
+    url = data['archives'][-1]
+    games = requests.get(url).json()
+    printer.pprint(games)
+
+get_player_rating('clashlaxboy')
+get_most_recent_game('clashlaxboy')
